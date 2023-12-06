2694ced5916d3643fdadddf9dec28b1ee8002faf
Cipick23 <133102658+Cipick23@users.noreply.github.com>
2023-12-06 16:30:14 +0200

    commit error images lightbox

--- src/gallery.js
+++ src/gallery.js
@@ -35,3 +35,39 @@ export function unpackPhotos(images) {
 
   return formattedImages;
 }
+
+
+// export function updateImagesList(markup) {
+//   const galleryContainer = document.querySelector('.gallery');
+//   if (galleryContainer) {
+//     galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+//     // Selectează toate imaginile adăugate recent în galerie
+//     const newImages = galleryContainer.querySelectorAll('.gallery a');
+
+//     // Adaugă eveniment de ascultare pentru fiecare imagine
+//     newImages.forEach(image => {
+//       image.addEventListener('click', event => {
+//         event.preventDefault();
+//       });
+//     });
+
+//     // Dacă există deja o instanță activă SimpleLightbox, distrugem instanța anterioară
+//     if (window.simpleLightboxInstance) {
+//       window.simpleLightboxInstance.destroy();
+//     }
+
+//     // Creează o nouă instanță SimpleLightbox pe toate imaginile
+//     window.simpleLightboxInstance = new SimpleLightbox(newImages, {
+//       // Opțiuni SimpleLightbox
+//       sourceAttr: 'href',
+//       captionsData: 'alt',
+//       captionPosition: 'bottom',
+//       captionDelay: 250,
+//       animationSpeed: 250,
+//       fadeSpeed: 300,
+//       showCounter: true,
+//       // Alte opțiuni pot fi adăugate aici
+//     });
+//   }
+// }
